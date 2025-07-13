a7797d9e9eecc85b05f6e4bdeb2af397ed18ea9c
ArKaDaTa000 <148015057+ArKaDaTa000@users.noreply.github.com>
2025-07-13 10:30:57 +0800

    Add files via upload

---
+++ Activity_4.java
@@ -0,0 +1,81 @@
+import java.util.*;
+public class Activity_4
+
+//lemonade serenade woop
+{
+    public static void main(String[]args)
+    {
+    Scanner sc1 = new Scanner(System.in);
+    String choices;
+
+    System.out.println("THIS IS YOUR ACTIVITY 4");
+
+    System.out.println("WOULD YOU LIKE TO PROCEED? PLEASE TYPE YES/NO");
+
+    while(true)
+    {
+    choices = sc1.nextLine().toUpperCase(); //all answer small will be correct{uppercase code}
+
+    switch(choices)
+     {
+     case "YES": //manifesting it works waaa
+
+     System.out.println("WELL THEN, THIS QUESTION IS WORTH 85 PTS:");
+     System.out.println("1. WHO FOUNDED THE JAVA PROGRAMMING LANGUAGE?");
+
+     Scanner sc2 = new Scanner(System.in);
+
+     String name1 = sc2.nextLine().toUpperCase();
+
+     if(name1.equals("JAMES GOSLING"))//java creator, first question?
+         {
+    System.out.println("GOOD JOB!, THE NEXT QUESTION IS WORTH 95 PTS:");
+
+    System.out.println("2. WHO WAS THE LEAD ARCHITECT, WHO HELPED DEVELOP C# PROGRAMMING LANGUAGE?");
+
+    Scanner sc3 = new Scanner(System.in);
+
+    String name2 = sc3.nextLine().toUpperCase(); // all capital letters answers again
+
+            if(name2.equals("ANDERS HEJLSBERG"))
+            {
+            System.out.println("BRAVO! THE NEXT QUESTION IS WORTH 100PTS:");
+
+            System.out.println("3. WHO CREATED THE PYTHON PROGRAMMING LANGUAGE?");
+
+            Scanner sc4 = new Scanner(System.in);
+
+            String name3 = sc4.nextLine().toUpperCase();
+
+                if(name3.equals("GUIDO VAN ROSSUM"))
+                {
+                System.out.println("CONGRATULATIONS. YOU ARE GIVEN 100PTS.");
+                System.exit(0);
+                }
+                else
+                {
+                System.out.println("SORRY. YOU ONLY ACCOMULATED 95 PTS.");
+                System.exit(0);
+                }
+            }
+            else
+            {
+            System.out.println("SORRY. YOU ONLY ACCOMULATED 85 PTS.");
+            System.exit(0);
+            }
+        }
+        else
+        {
+        System.out.println("SORRY. YOU ONLY ACCOMULATED 75 PTS.");
+        System.exit(0);
+        }
+    return;
+    case "NO":
+    System.exit(0); //exit means to automatically exit -thankyuu jas
+    default:
+    System.out.println("PLEASE FOLLOW THE INSTRUCTION, PLEASE TYPE YES/NO");
+    break;
+     }
+    }
+}
+}
